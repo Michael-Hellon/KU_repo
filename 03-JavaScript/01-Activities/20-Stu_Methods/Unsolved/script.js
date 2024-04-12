0d8046e1f1c3af1a332967889ce13230484840bc
Michael Hellon <kansasboy308@gmail.com>
2024-04-11 21:59:55 -0500

    udated files inclass  Changes to be committed: 	renamed:    01-HTML-Git-CSS/01-Activities/08-Stu_Attributes/Unsolved/assets/images/image-Michael.jpg -> 01-HTML-Git-CSS/01-Activities/08-Stu_Attributes/Unsolved/assets/images/Michael.jpg 	modified:   01-HTML-Git-CSS/01-Activities/08-Stu_Attributes/Unsolved/index.html 	modified:   03-JavaScript/01-Activities/16-Stu_Functions/Unsolved/script.js 	modified:   03-JavaScript/01-Activities/18-Stu_Scope/Unsolved/script.js 	modified:   03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js 	modified:   03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js

--- 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,18 @@ const planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 const star = "polaris";
 
 // WRITE YOUR CODE BELOW
+
+constellations.unshift('Canis Major');
+console.log(constellations);
+
+planets.pop();
+console.log(planets);
+
+const galaxy = constellations.concat(planets);
+console.log(galaxy);
+console.log(constellations);
+console.log(planets);
+
+const newStar = star.toUpperCase()
+console.log(newStar);
+
--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,24 @@
 // WRITE YOUR CODE BELOW
+const customerDrink = {
+    name: "Coffee",
+    sugars: 3,
+    isReady: true,
+};
+
+console.log(customerDrink)
+
+
+
+// if(customerDrink.isReady === true) {
+//     console.log('Ready for pick-up');
+// } else {
+//     console.log('Still in order queue');
+// };
+
+// or
+
+if (customerDrink.isReady === true) {
+    console.log(`Ready for pick-up: ${customerDrink.name} with ${customerDrink.sugars} sugars.`);
+} else {
+    console.log(`Still in order queue: ${customerDrink.name} with ${customerDrink.sugars} sugars.`);
+}
