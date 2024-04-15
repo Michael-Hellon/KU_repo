b24d592a4ea3179c2b79a4e54077b9947ee809c5
Michael Hellon <kansasboy308@gmail.com>
2024-04-15 18:08:15 -0500

    update js comments for in class exercise Changes to be committed: 	modified:   04-Web-APIs/01-Activities/01-Ins_Window-Object/script.js 	modified:   04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js 	modified:   04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/index.html 	modified:   04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/script.js

--- 04-Web-APIs/01-Activities/01-Ins_Window-Object/script.js
+++ 04-Web-APIs/01-Activities/01-Ins_Window-Object/script.js
@@ -1,11 +1,11 @@
 // Logs window object using this
-console.log("this: ");
-console.log(this);
+console.log("this: "); // logs the word-  this: 
+console.log(this); // this is a variable - logs the value of this 
 
 // Logs the document object
-console.log("window.document: ");
-console.log(window.document);
+console.log("window.document: "); // logs the words- window.document
+console.log(window.document);   // variable - logs the value of window.document
 
 // Logs body of document
-console.log("document.body: ");
-console.log( document.body);
+console.log("document.body: "); // logs the word- document.body:
+console.log( document.body);  // variable - logs the value of document.body
--- 04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/script.js
+++ 04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/script.js
@@ -1,18 +1,19 @@
 console.log('Document Body: ');
-console.log(document.body);
+console.log(document.body); // logs the <body> portion of the code
 
 console.log('Children of Document Body: ');
-console.log(document.body.children);
+console.log(document.body.children); // logs the <tags> within the <body></body>
 
 console.log('First child of body: ');
-console.log(document.body.children[0]);
+console.log(document.body.children[0]); //<h1>Open the Console to See the Magic ✨! </h1>
 
 console.log('First child of the ul: ');
-console.log(document.body.children[1].children[0]);
+console.log(document.body.children[1].children[0]); // <li id ="first-child-ul">Children[0]</li>
 
 // Accessing element by id
 const firstChildUl = document.getElementById('first-child-ul');
-console.log(firstChildUl);
+console.log('A', firstChildUl, 'A'); // <li id="first-child-ul" style="color: green;">Children[0]</li>
+ // pulls the element.style { color: green; } from the elements WEB DEV page (f12)
 
 // Setting style of element
 firstChildUl.style.color = 'green';
