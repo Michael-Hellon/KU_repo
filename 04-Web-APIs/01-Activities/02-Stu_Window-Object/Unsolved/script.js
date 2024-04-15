ba5eb4dc16a4fb25723d1ac460278219fef1a8d7
Michael Hellon <kansasboy308@gmail.com>
2024-04-15 11:02:03 -0500

    Update script.js

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,16 @@
 // TODO: What will the following line of code log?
+//window
 console.log(window);
 
 // TODO: What will the following line of code log?
+// window.document
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+//document.body
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
-console.log(document.head);
+
+console.log('2',document.head);
 
