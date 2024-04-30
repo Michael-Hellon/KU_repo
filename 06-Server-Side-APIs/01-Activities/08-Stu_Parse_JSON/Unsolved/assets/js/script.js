19eab7827e5c7ce38baec61d05c0b4ffff8e3f7b
Michael Hellon <kansasboy308@gmail.com>
2024-04-29 21:32:16 -0500

    Updated in class Changes to be committed: modified:   06-Server-Side-APIs/01-Activities/04-Stu_Create_Fetch/Unsolved/assets/js/script.js modified:   06-Server-Side-APIs/01-Activities/06-Stu_Reverse_APIs/Unsolved/assets/js/script.js modified:   06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js

--- 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -1,5 +1,5 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-const requestUrl = 'https://api.github.com/repos/{owner}/{repo}/issues';
+const requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
 
 fetch(requestUrl)
   .then(function (response) {
@@ -8,6 +8,10 @@ fetch(requestUrl)
   .then(function (data) {
     console.log('Github Repo Issues \n----------');
     console.log(data);
+    for (issue of data) {
+      console.log(issue.url);
+      console.log(issue.user.login);
+    }
     // TODO: Loop through the response
     // TODO: Console log each issue's URL and each user's login
   });
