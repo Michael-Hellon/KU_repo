70ee9f0df7c2a483ea49df6a755ba03b4feda6d7
Michael Hellon <kansasboy308@gmail.com>
2024-06-07 13:52:23 -0500

    updated files for class

--- 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
+++ 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
@@ -6,19 +6,31 @@ const app = express();
 
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
-  // Your code here
+  const msg = `${req.method} request received to get reviews` 
+  res.json(msg)
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
 // Your code here
+app.post('/api/reviews', (req, res) => {
+  const msg = `${req.method} request received to get reviews` 
+  res.json(msg)
+  console.log(msg);
+});
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
-  // Your code here
+  const msg = `${req.method} request received to get reviews` 
+  res.json(msg)
+  console.log(msg)
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
-// Your code here
+app.post('/api/reviews', (req, res) => {
+  const msg = `${req.method} request received to get reviews` 
+  res.json(msg)
+  console.log(msg);
+});
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
