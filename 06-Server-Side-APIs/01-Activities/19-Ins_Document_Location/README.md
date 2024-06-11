671848f996c154660861eec25fbf5d1a44d57fcb
Michael Hellon <kansasboy308@gmail.com>
2024-06-10 20:32:29 -0500

    updated files
    VSC is running slow

--- 06-Server-Side-APIs/01-Activities/19-Ins_Document_Location/README.md
+++
@@ -1,9 +0,0 @@
-Instruction will demonstrate this in the browser console.
-https://developer.mozilla.org/en-US/docs/Web/API/Document/location
-
-console.log(document.location)
-
-<!-- The Document.location read-only property returns a Location object, which contains information about the URL of the document and provides methods for changing that URL and loading another URL. -->
-
-<!-- Though Document.location is a read-only Location object, you can also assign a string to it. This means that you can work with document.location as if it were a string in most cases: document.location = 'http://www.example.com' is a synonym of document.location.href = 'http://www.example.com'. If you assign another string to it, browser will load the website you assigned. -->
-
--- 11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
+++ 11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 // TODO: Require the json file located in `/db`
-const data = require('./db/repos.json')
+// Require the json file located in `/db`
+const data = require('./db/repos.json');
+
 // TODO: Create an `app` variable set to the value of `express()`
-const app = express()
+// Create an `app` variable set to the value of `express()`
+const app = express();
 
 app.get('/', (req, res) => {
   res.send(
-    'Use the API endpoint at <a href="http://localhost:3001/api">localhost:3001/api</a>'
   );
 });
 
 // TODO: Create a GET route for `/api` that will return the content of our json file
-app.get('api', (req, res) => res.json(data)) 
+// Create a GET route for `/api` that will return the content of our json file
+app.get('/api', (req, res) => res.json(data));
+
 // TODO: Have the app listen on port 3001
-app.listen(3001, () => console.log(' Listening at https://localhost:3001'))
+// Have the app listen on port 3001
+app.listen(3001, () => console.log(`Listening at http://localhost:3001`));
